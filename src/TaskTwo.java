066d462011ceb5e52486fa236cbb09302846e0ce
Andrey-kr89 <andrey.kri89@gmail.com>
2021-08-23 11:30:49 +0300

    TaskTwo corrected

--- src/TaskTwo.java
+++ src/TaskTwo.java
@@ -2,6 +2,7 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class TaskTwo {
@@ -10,34 +11,30 @@ public class TaskTwo {
         Gson jsonFile = new GsonBuilder().setPrettyPrinting().create();
         File inputFile = new File("file2.txt");
         Scanner inpFromFile = new Scanner(inputFile);
-        String s = new String();
-        while (inpFromFile.hasNextLine()) {
-            String input = inpFromFile.nextLine();
-            String[] arr;
-            if (input.length() > 2) {
-                arr = input.split(" ");
-            } else continue;
-            if (!arr[1].matches("^-?\\d+$")) continue;
-            Person person = new Person();
-            person.setName(arr[0]);
-            person.setAge(Integer.parseInt(arr[1]));
-            s = s + jsonFile.toJson(person) + "\n";
-            try (FileWriter writer = new FileWriter("namesJson.txt")) {
-                writer.write(s);
-                writer.flush();
+        int i = 0;
+        ArrayList<Person> persons = new ArrayList<Person>();
+        try (FileWriter writer = new FileWriter("namesJson.txt")) {
+            while (inpFromFile.hasNextLine()) {
+                Person person = new Person();
+                String input = inpFromFile.nextLine();
+                String[] arr;
+                if (input.length() > 2) {
+                    arr = input.split(" ");
+                } else continue;
+                if (!arr[1].matches("^-?\\d+$")) continue;
+                person.setName(arr[0]);
+                person.setAge(Integer.parseInt(arr[1]));
+                persons.add(person);
+                i++;
             }
+            writer.append(jsonFile.toJson(persons));
+            writer.flush();
         }
     }
 }
 
+
 class Person {
-    @Override
-    public String toString() {
-        return "Person{" +
-                "name='" + name + '\'' +
-                ", age=" + age +
-                '}';
-    }
 
     private String name;
     private int age;
